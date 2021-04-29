557a8fafe43acb9810e8c4cc1ff5794904b9994d
e4t4g <evahillertyan@gmail.com>
2021-04-29 19:43:27 +0300

    new update

--- 1task4lesson.go
+++ 1task4lesson.go
@@ -1,6 +1,7 @@
-package main
+/*package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"os/signal"
@@ -10,18 +11,15 @@ import (
 
 func main() {
 
-	ch:=make(chan os.Signal,1)
+	ch := make(chan os.Signal, 1)
 	signal.Notify(ch, syscall.SIGTERM, syscall.SIGINT)
-
 
 	go func() {
 		for sig := range ch {
 			switch sig {
-			case syscall.SIGTERM:
+			case syscall.SIGTERM, syscall.SIGINT:
 				fmt.Println("SIGTERM received")
-				os.Exit(0)
-			case syscall.SIGINT:
-				fmt.Println("SIGINT received")
+				<-time.After(1 * time.Second)
 				os.Exit(0)
 			}
 		}
@@ -30,7 +28,47 @@ func main() {
 	func() {
 		for {
 			fmt.Println(time.Now().Format("15:04:05 "))
-			time.Sleep(1*time.Second)
+			time.Sleep(1 * time.Second)
 		}
 	}()
 }
+*/
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	ch:=make(chan os.Signal,1)
+	signal.Notify(ch, syscall.SIGTERM, syscall.SIGINT)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1 * time.Second)
+	defer cancel()
+
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				for {
+					fmt.Println(time.Now().Format("15:04:05 "))
+					time.Sleep(1 * time.Second)
+				}
+			}
+		}
+	}()
+
+	sign := <- ch
+	fmt.Println("SIGTERM received", sign)
+}
+
+
