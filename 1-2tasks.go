72de935739340cce8f3b230890ec29fc4c63b0d2
e4t4g <evahillertyan@gmail.com>
2021-05-02 18:26:07 +0300

    update

--- 1-2tasks.go
+++ 1-2tasks.go
@@ -9,17 +9,19 @@ import (
 func main() {
 	const n int = 10
 	wg := sync.WaitGroup{}
-	sl := make([]int, 0)
-	var lock sync.Mutex
 	var j int
-
+	var (
+		lock sync.Mutex
+		sl = make([]int, 0)
+	)
+	
 	//Implementation of Mutex
 	for i := 0; i < n; i++ {
 		wg.Add(1)
 		go func(i int, lock *sync.Mutex) {
-			defer wg.Done()
-			defer lock.Unlock()
 			lock.Lock()
+			defer lock.Unlock()
+			defer wg.Done()
 			sl = append(sl, i)
 		}(i, &lock)
 	}
